3b2733d843b00adf32653c1b64c1bc2ae1e7ec9c
Jr.Timmothy <112587120+ANormalDude14@users.noreply.github.com>
2022-12-09 01:50:46 -0800

    Create If-Statements.lua

---
+++ If-Statements.lua
@@ -0,0 +1,32 @@
+--Looks Like Youve Completed The Introduction, Lets Dive More Deep Into ROBLOX Development With Your 2nd Lesson--
+
+--What Are If-Statements--?:
+
+--If Statements Basically Checks Something, Integers, Strings Or Booleans--
+--Specificaly Used For A Little Advanced Logic--
+
+--Heres An Example Of An If-Statement--:
+
+local Integer = 15
+
+if Integer > 15 then --The > Sign Is Used If A Value Is More Greater Than A Other Value Or < Sign Is Used For Value Less Greater Than The Other Value
+    print("Integer Is More Than 15") --Gets Output After We Checked If Integer > 15
+end
+--What Does It Do---:
+
+--In Line 13, Inside That Little Container Is Our Code If Our Statement Is > 15--
+--So If-Statements Are Like This As A Little Example--:
+
+--John: Hmm There Are 5 Sets Of Fruit On The Table
+--John: If One Fruit Is Not The Same As The Others Lets Throw It Out
+
+--Those Are If-Statements To Be More Friendly To You--
+--To Let You Get An Understanding, Im Gonna Give Another Example Of If-Statement--:
+
+local Apples = 5
+if Apples < 5 then
+    print("There Are 5 Apples")   
+else
+    print("Someone Ate A Apple")
+end
+--Thats Gonna Be The End Of Our Lesson, Hopefully Youve Messed Around With If-Statements To Get The Feel For It--
